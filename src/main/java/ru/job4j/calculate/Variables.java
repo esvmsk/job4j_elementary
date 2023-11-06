2f606510bae8867c22ae8cc8a260de0b7dc25fa9
SE <esvmsk980@gmail.com>
2023-11-06 08:47:58 +0300

    0. Что такое переменная. [#3610]

--- src/main/java/ru/job4j/calculate/Variables.java
+++ src/main/java/ru/job4j/calculate/Variables.java
@@ -2,7 +2,7 @@ package ru.job4j.calculate;
 
 public class Variables {
     public static void main(String[] args) {
-        int age = 19;
+        int age = 18;
         System.out.println(age);
         float length = 78.0332F;
         System.out.println(length);
