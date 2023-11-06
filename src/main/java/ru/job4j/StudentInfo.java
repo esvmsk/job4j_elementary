9fd93220d50937407eec0e6029ade4b973735e08
SE <esvmsk980@gmail.com>
2023-11-06 22:31:18 +0300

    6. Checkstyle [#4123 #457998]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -2,7 +2,7 @@ package ru.job4j;
 
 public class StudentInfo {
     public static void main(String[] args) {
-        System.out.println("MARIYA");
+        System.out.println("MARI");
         System.out.println("30.08.2013");
         System.out.println("1*2=2");
         System.out.println("1*3=3");
